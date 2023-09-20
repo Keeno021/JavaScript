0a5975d0b6e1bed93a9e946bbfbc57ba7de3e049
Keeno <keenosdev@gmail.com>
2023-09-20 13:42:43 +0200

    calculate age in dog years

---
+++ Dog years/app.js
@@ -0,0 +1,25 @@
+// defining my age
+const myAge = 20;
+// defining earlyYears
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+// number of dog years accounted for by your later years
+laterYears *= 4;
+
+// age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+
+//tring with all lowercase letters
+myName = "Keeno".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+
+/*
+var greeting = function() {
+    console.log('Hello World!');  
+};
+  
+arrow functions
+const greeting = () => console.log('Hello World'); 
+*/
